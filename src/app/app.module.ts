3cca2ecbae92c6305da70c2b6b423299d485930d
Kurt Khris <48037522+KurtKhris@users.noreply.github.com>
2021-07-02 11:57:13 +0000

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
@@ -10,19 +16,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
-import { HomeComponent } from './home';
+import { HomeComponent } from './home';;
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+;
+import { Covid19Component } from './covid19/covid19.component'
 
 @NgModule({
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
-        AppRoutingModule
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatInputModule,
+        FormsModule,
+        MatIconModule,
+        MatFormFieldModule,
     ],
     declarations: [
         AppComponent,
         AlertComponent,
-        HomeComponent
+        HomeComponent,
+        Covid19Component
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
---
+++ src/app/covid19/covid19.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { CountryReports } from 'src/countryReports';
+import { ApiServicesService } from '../api-services.service'
+
+@Component({
+  selector: 'app-covid19',
+  templateUrl: './covid19.component.html',
+  styleUrls: ['./covid19.component.less']
+})
+export class Covid19Component implements OnInit {
+  ELEMENT_DATA! : CountryReports[];
+  displayedColumns: string[] = ['country', 'cases', 'todayCases', 'deaths', 'todayDeaths', 'recovered', 'active', 'critical', 'casesPerOneMillion', 'deathsPerOneMillion', 'tests', 'testsPerOneMillion' ];
+  dataSource = new MatTableDataSource<CountryReports>(this.ELEMENT_DATA);
+
+
+   //Pagination
+   @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+   @ViewChild(MatSort, {static: true}) sort!: MatSort;
+  constructor(private service: ApiServicesService) { }
+
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.getAllReports();
+  }
+
+  public getAllReports(){
+    let res=this.service.covid19Reports();
+    res.subscribe(report=>this.dataSource.data=report as CountryReports[]);
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+}
